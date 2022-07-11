dfc91463f23c3e16fdf256980e137b8dcfb26bde
nictes1 <nikolastesone@gmail.com>
2022-07-10 23:37:47 -0300

    add class 3

---
+++ goweb/cmd/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error al intentar cargar archivo .env")
+	}
+
+	db := store.NewStore("products.json")
+	if err := db.Ping(); err != nil {
+		log.Fatal("error al intentar cargar archivo")
+	}
+
+	repo := products.NewRepository(db)
+	s := products.NewService(repo)
+	p := handler.NewProduct(s)
+
+	r := gin.Default()
+	pr := r.Group("products")
+	pr.GET("/", p.GetAll())
+	pr.POST("/", p.Store())
+	pr.PUT("/:id", p.Update())
+	pr.PATCH("/:id", p.UpdateName())
+	pr.DELETE("/:id", p.Delete())
+
+	r.Run()
+}
