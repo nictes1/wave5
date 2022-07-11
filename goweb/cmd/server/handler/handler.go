86fae5f1386fc66690cf18b79fa092e08a46a3fb
nictes1 <nikolastesone@gmail.com>
2022-07-11 00:01:04 -0300

    add response message && request validation - class 4 tm

--- goweb/cmd/server/handler/handler.go
+++ goweb/cmd/server/handler/handler.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/nictes1/wave5/goweb/internal/products"
+	"github.com/nictes1/wave5/goweb/pkg/web"
 )
 
 type request struct {
@@ -138,59 +139,54 @@ func (p *Product) GetAll() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			c.JSON(401, gin.H{"error": "Token inválido"})
+			c.JSON(401, web.NewResponse(http.StatusUnauthorized, nil, "Error: Token invalido"))
 			return
 		}
 
 		p, err := p.service.GetAll()
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 			return
 		}
-		c.JSON(200, p)
+		c.JSON(200, web.NewResponse(http.StatusOK, p, ""))
 	}
 }
 
 func (p *Product) Store() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		token := c.GetHeader("token")
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
 			return
 		}
 
 		var req request
-		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.JSON(400, web.NewResponse(400, nil, err.Error()))
 			return
 		}
-
 		if req.Name == "" {
-			c.JSON(400, gin.H{"error": "El nombre del producto es requerido"})
+			ctx.JSON(400, web.NewResponse(400, nil, "El nombre del producto es requerido"))
 			return
 		}
-
 		if req.Type == "" {
-			c.JSON(400, gin.H{"error": "El tipo de producto es requerido"})
+			ctx.JSON(400, web.NewResponse(400, nil, "El tipo del producto es requerido"))
 			return
 		}
-
 		if req.Count == 0 {
-			c.JSON(400, gin.H{"error": "La cantidad es requerido"})
+			ctx.JSON(400, web.NewResponse(400, nil, "La cantidad es requerida"))
 			return
 		}
-
 		if req.Price == 0 {
-			c.JSON(400, gin.H{"error": "El precio es requerido"})
+			ctx.JSON(400, web.NewResponse(400, nil, "El precio es requerido"))
 			return
 		}
-
 		p, err := p.service.Store(req.Name, req.Type, req.Count, req.Price)
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(400, web.NewResponse(400, nil, err.Error()))
 			return
 		}
-
-		c.JSON(200, gin.H{"products": p})
+		ctx.JSON(200, web.NewResponse(200, p, ""))
 	}
+
 }
